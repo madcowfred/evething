34cb7016ba2ce8eeac535c09c020741daf61ab3e
Freddie <freddie@madcowdisease.org>
2010-07-06 00:31:58 +0930

    Admin page changes for usability

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,5 +1,13 @@
-from everdi.blueprints.models import BlueprintCopy, Character
+from everdi.blueprints.models import BlueprintInstance, Character
 from django.contrib import admin
 
-admin.site.register(Character)
-admin.site.register(BlueprintCopy)
+class CharacterAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['user']}),
+		('Character information', {'fields': ['name', 'api_key', 'industry_skill', 'production_efficiency_skill']}),
+		('Factory costs', {'fields': ['factory_cost', 'factory_per_hour']}),
+		('Taxes', {'fields': ['sales_tax', 'brokers_fee']}),
+	]
+
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(BlueprintInstance)
