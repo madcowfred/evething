e498dced42d479912d22e745d9151b509846c418
Freddie <freddie@madcowdisease.org>
2010-07-06 00:31:39 +0930

    Model changes, hopefully done for now

--- blueprints/models.py
+++ blueprints/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Character(models.Model):
+	user = models.ForeignKey(User)
 	name = models.CharField(max_length=30)
 	api_key = models.CharField(max_length=64)
 	industry_skill = models.IntegerField()
@@ -39,21 +41,34 @@ class ItemPrice(models.Model):
 class Blueprint(models.Model):
 	id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=128)
-	components = models.ManyToManyField(Item, through='BlueprintComponent')
+	item = models.ForeignKey(Item)
+	components = models.ManyToManyField(Item, through='BlueprintComponent', related_name='components')
 	
 	production_time = models.IntegerField()
+	productivity_modifier = models.IntegerField()
+	material_modifier = models.IntegerField()
 	waste_factor = models.IntegerField()
 	
 	def __unicode__(self):
 		return self.name
+	
+	class Meta:
+		ordering = ('name',)
 
 class BlueprintComponent(models.Model):
 	blueprint = models.ForeignKey(Blueprint)
 	item = models.ForeignKey(Item)
 	count = models.IntegerField()
+	needs_waste = models.BooleanField()
 
-class BlueprintCopy(models.Model):
+class BlueprintInstance(models.Model):
 	character = models.ForeignKey(Character)
 	blueprint = models.ForeignKey(Blueprint)
 	material_level = models.IntegerField()
 	productivity_level = models.IntegerField()
+	
+	def __unicode__(self):
+		return "%s's %s" % (self.character.name, self.blueprint.name)
+	
+	class Meta:
+		ordering = ('blueprint',)
