8dc05c7848bbb3d8d4ec5e431557c7b1a6377178
Freddie <freddie@madcowdisease.org>
2010-09-09 19:09:18 +0930

    Turns out we should fix this too.

--- thing/sql/corporation.sql
+++ thing/sql/corporation.sql
@@ -1,2 +1,2 @@
-INSERT INTO rdi_corporation (id, name) VALUES (751993277, 'Rubber Ducky Space Industries');
-INSERT INTO rdi_corporation (id, name) VALUES (363368013, 'Eighty Joule Brewery');
+INSERT INTO thing_corporation (id, name) VALUES (751993277, 'Rubber Ducky Space Industries');
+INSERT INTO thing_corporation (id, name) VALUES (363368013, 'Eighty Joule Brewery');
