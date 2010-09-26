9e1dfc1b90190b2b44af01f9166ade90f17b68a9
Freddie <freddie@madcowdisease.org>
2010-09-26 09:58:50 +0930

    Shift our custome tablesorter parser into its own file.

---
+++ thing/media/thing.js
@@ -0,0 +1,23 @@
+$.tablesorter.addParser({
+	id: 'human',
+	is: function(s) {
+		return /^[0-9.]+[KMB]?$/.test(s);
+	},
+	format: function(s) {
+		var l = s.length - 1;
+		var c = s.charAt(l);
+		if (c == 'K') {
+			return s.substr(0, l) * 1000;
+		}
+		else if (c == 'M') {
+			return s.substr(0, l) * 1000000;
+		}
+		else if (c == 'B') {
+			return s.substr(0, l) * 1000000000;
+		}
+		else {
+			return s;
+		}
+	},
+	type: 'numeric',
+});
