d18e7229444fc4bd64f078cf6e07764886b1d78e
Freddie <freddie@madcowdisease.org>
2010-11-18 08:19:41 +1030

    Turns out this shouldn't just be commented out.

--- thing/views.py
+++ thing/views.py
@@ -349,8 +349,8 @@ def trade_timeframe(request, year=None, month=None, period=None, slug=None):
 		if item.diff > 0:
 			item_row['projected_average'] = item.balance + (item.diff * item.sell_average)
 			item_row['outstanding_average'] = item_row['projected_average'] - item.balance
-			#if item.sell_price:
-			#	item_row['projected_market'] = item.balance + (item.diff * item.sell_price)
+			if item.sell_price:
+				item_row['projected_market'] = item.balance + (item.diff * item.sell_price)
 		else:
 			item_row['projected_average'] = item.balance
 			item_row['projected_market'] = item.balance
