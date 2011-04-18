059f808519c312402abe1c48366249491ec06ee5
Freddie <freddie@madcowdisease.org>
2011-04-19 00:45:17 +0930

    Switch history updater to using eve-metrics.

--- history_updater.py
+++ history_updater.py
@@ -1,6 +1,10 @@
 #!/usr/local/bin/python
 
+import bz2
+import csv
+import datetime
 import os
+import psycopg2
 import time
 import urllib2
 import xml.etree.ElementTree as ET
@@ -10,13 +14,54 @@ from django.core.management import setup_environ
 import settings
 setup_environ(settings)
 
-from thing.models import *
+from decimal import *
+
+#from local_settings import DATABASES
+#from thing.models import *
 
 
-HISTORY_URL = 'http://eve-metrics.com/api/history.xml?type_ids=%s&days=90'
+#HISTORY_URL = 'http://eve-metrics.com/api/history.xml?type_ids=%s&days=90'
+HISTORY_FILE = '10000002.csv.bz2'
+
 
 
 def main():
+	conn = psycopg2.connect('dbname=%(NAME)s user=%(USER)s' % (settings.DATABASES['default'])) 
+	cur = conn.cursor()
+	
+	print time.time()
+	
+	histf = bz2.BZ2File(HISTORY_FILE)
+	reader = csv.reader(histf)
+	reader.next()
+	
+	current_id = None
+	history = []
+	for type_id, orders, movement, price_max, price_avg, price_min, date in csv.reader(histf):
+		if current_id == type_id:
+			history.append((int(type_id), date, int(orders), int(movement), Decimal(price_max), Decimal(price_avg), Decimal(price_min)))
+		else:
+			# Get the last history entry for this type_id
+			cur.execute('SELECT date FROM thing_itempricehistory WHERE item_id = %s ORDER BY date DESC LIMIT 1', (current_id,))
+			row = cur.fetchone()
+			# If there is a latest database entry, filter anything older from history list
+			if row is not None:
+				last_date = '%s-%02d-%02d' % (row[0].year, row[0].month, row[0].day)
+				history = [h for h in history if h[0] > last_date]
+			
+			print type_id, len(history)
+			history.sort()
+			cur.executemany('INSERT INTO thing_itempricehistory (item_id, date, orders, movement, maximum, average, minimum) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+				history)
+			conn.commit()
+			
+			# Reset stuff
+			current_id = type_id
+			history = []
+	
+	print time.time()
+	return
+	
 	# Get a list of items required to construct blueprint instances - I apologise, this is horrible
 	item_ids = set()
 	for bp_id in BlueprintInstance.objects.values_list('blueprint').distinct():
