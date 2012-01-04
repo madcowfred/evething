7f32e37fca680382bd4d0ed9b8b12437d5131c66
Freddie <freddie@madcowdisease.org>
2012-01-04 14:04:38 +1030

    Remove the useless DATABASES definition in settings.py

--- settings.py
+++ settings.py
@@ -9,18 +9,6 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
-# Define this in local_settings.py you goof
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': '/home/freddie/git/evething/evething.db',                      # Or path to database file if using sqlite3.
-#        'USER': '',                      # Not used with sqlite3.
-#        'PASSWORD': '',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
