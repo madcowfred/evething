c563f158ca233dea3cedc867c417aa6ffa8c34a9
Freddie <freddie@madcowdisease.org>
2012-02-05 04:51:04 +1030

    Move WSGI setup to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append('/home/freddie/git')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'evething.settings'
 
 import django.core.handlers.wsgi
