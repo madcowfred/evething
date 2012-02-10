9a6690dd7e329e5559f9bda6caef3f016dc5041b
Freddie <freddie@wafflemonster.org>
2012-02-10 16:47:03 +1030

    Need this too, oops

---
+++ thing/sql/characterskillintraining.postgresql_psycopg2.sql
@@ -0,0 +1,2 @@
+ALTER TABLE thing_characterskillintraining ALTER COLUMN start_time TYPE timestamp without time zone;
+ALTER TABLE thing_characterskillintraining ALTER COLUMN end_time TYPE timestamp without time zone;
