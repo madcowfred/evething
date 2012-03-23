65fc92a36f243cb3ec3fe85827d69f21b1301640
Freddie <freddie@wafflemonster.org>
2012-03-23 20:38:35 +1030

    Delete ancient unused Django app

--- buyme/price_override.py
+++
@@ -1,6 +0,0 @@
-from decimal import *
-
-# Override prices here
-PRICE_OVERRIDE = {
-	# 34: Decimal('2.5'),
-}
--- buyme/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('everdi.buyme.views',
-	(r'^$',			'form'),
-	(r'^calc/$',	'calc'),
-)
