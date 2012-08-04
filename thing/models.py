c3e9e6988e6191e6758c66306bfc887af5026256
Freddie <freddie@wafflemonster.org>
2012-08-04 17:42:42 +0930

    Add assignee_alliance to Contract model + view support

--- thing/models.py
+++ thing/models.py
@@ -696,6 +696,7 @@ class Contract(models.Model):
     issuer_corp = models.ForeignKey(Corporation, related_name="contract_issuers")
     assignee_char = models.ForeignKey(SimpleCharacter, blank=True, null=True, related_name="contract_assignees")
     assignee_corp = models.ForeignKey(Corporation, blank=True, null=True, related_name="contract_assignees")
+    assignee_alliance = models.ForeignKey(Alliance, blank=True, null=True, related_name="contract_assignees")
     acceptor_char = models.ForeignKey(SimpleCharacter, blank=True, null=True, related_name="contract_acceptors")
     acceptor_corp = models.ForeignKey(Corporation, blank=True, null=True, related_name="contract_acceptors")
 
