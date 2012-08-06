d7ff6afb93f65168ab86d9e6387fe23ff91635a7
Freddie <freddie@wafflemonster.org>
2012-08-06 19:04:10 +0930

    Don't need the sites app

--- evething/settings.py
+++ evething/settings.py
@@ -103,7 +103,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
