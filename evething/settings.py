615f8c350ecea483f4276ab41be31c5297c66aa7
Freddie <freddie@wafflemonster.org>
2012-09-03 20:24:05 +0930

    Rename base_static/ to static/, it annoys me

--- evething/settings.py
+++ evething/settings.py
@@ -61,7 +61,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(_PATH, 'base_static'),
+    os.path.join(_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
