e28122ae30f3c92c4391c5242eb25ad329aab74c
Freddie <freddie@wafflemonster.org>
2012-09-15 19:59:02 +0930

    Modify wsgi.py for Django 1.4

--- wsgi.py
+++ wsgi.py
@@ -9,12 +9,17 @@ except ImportError:
 else:
     sys.modules["decimal"] = cdecimal
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'evething.settings'
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'evething.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evething.settings')
 
 import djcelery
 djcelery.setup_loader()
 
-import django.core.handlers.wsgi
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
 
-application = django.core.handlers.wsgi.WSGIHandler()
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
