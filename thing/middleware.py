2a3ffa2e8bde58d4e3dec053ad437842eac80106
Freddie <freddie@wafflemonster.org>
2012-12-09 17:42:51 +1030

    Change LastSeenMiddleware to store last_seen as utcnow() instead of now()

--- thing/middleware.py
+++ thing/middleware.py
@@ -5,6 +5,6 @@ class LastSeenMiddleware(object):
         if not request.user.is_authenticated():
             return None  
         profile = request.user.get_profile()
-        profile.last_seen = datetime.datetime.now()
+        profile.last_seen = datetime.datetime.utcnow()
         profile.save()
         return None
