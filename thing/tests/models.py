aa68f0f38ae52d09a7684bedaf944a5aff755982
Freddie <freddie@wafflemonster.org>
2012-12-26 03:15:59 +1030

    Add BlueprintInstance tests to verify that production costs/times are calculated correctly

--- thing/tests/models.py
+++ thing/tests/models.py
@@ -1,3 +1,5 @@
+from decimal import *
+
 from django.test import TestCase
 from thing.models import *
 
@@ -57,3 +59,67 @@ class InventoryFlagTestCase(TestCase):
         
         self.assertEqual(flags, [self.hi_slot_1, self.med_slot_1, self.low_slot_1, self.rig_slot_1,
             self.drone_bay, self.ship_hangar, self.fuel_bay])
+
+class BlueprintTestCase(TestCase):
+    fixtures = ['blueprint_testdata.json']
+
+    def setUp(self):
+        super(BlueprintTestCase, self).setUp()
+
+        self.maelstrom_neg4_neg4 = BlueprintInstance.objects.create(
+            blueprint_id=24695,
+            original=True,
+            material_level=-4,
+            productivity_level=-4,
+        )
+
+        self.maelstrom_0_0 = BlueprintInstance.objects.create(
+            blueprint_id=24695,
+            original=True,
+            material_level=0,
+            productivity_level=0,
+        )
+
+        self.maelstrom_50_10 = BlueprintInstance.objects.create(
+            blueprint_id=24695,
+            original=True,
+            material_level=50,
+            productivity_level=10,
+        )
+
+        self.comps_neg4_neg4 = self.maelstrom_neg4_neg4._get_components()
+        self.comps_0_0 = self.maelstrom_0_0._get_components()
+        self.comps_50_10 = self.maelstrom_50_10._get_components()
+
+    def test_calc_production_time(self):
+        self.assertEqual(self.maelstrom_neg4_neg4.calc_production_time(), 28800)
+        self.assertEqual(self.maelstrom_0_0.calc_production_time(), 14400)
+        self.assertEqual(self.maelstrom_50_10.calc_production_time(), 11782)
+
+    def test_calc_production_cost(self):
+        # Production cost from buys
+        self.assertEqual(self.maelstrom_neg4_neg4.calc_production_cost(), Decimal('319598442.95'))
+        self.assertEqual(self.maelstrom_0_0.calc_production_cost(), Decimal('234372093.37'))
+        self.assertEqual(self.maelstrom_50_10.calc_production_cost(), Decimal('213480621.36'))
+        
+        # Production cost from sells
+        self.assertEqual(self.maelstrom_neg4_neg4.calc_production_cost(use_sell=True), Decimal('324144004.04'))
+        self.assertEqual(self.maelstrom_0_0.calc_production_cost(use_sell=True), Decimal('237705504.90'))
+        self.assertEqual(self.maelstrom_50_10.calc_production_cost(use_sell=True), Decimal('216516866.94'))
+
+    def test_get_components(self):
+        comps = [(item.name, n) for item, n in self.comps_neg4_neg4]
+        comps.sort(key=lambda c: c[1])
+        self.assertEqual(comps, [('Megacyte', 3413), ('Zydrine', 14801), ('Nocxium', 62129), ('Isogen', 248777), ('Mexallon', 995930),
+            ('Pyerite', 3982146), ('Tritanium', 15926640)])
+
+        comps = [(item.name, n) for item, n in self.comps_0_0]
+        comps.sort(key=lambda c: c[1])
+        self.assertEqual(comps, [('Megacyte', 2503), ('Zydrine', 10854), ('Nocxium', 45561), ('Isogen', 182436), ('Mexallon', 730348),
+            ('Pyerite', 2920240), ('Tritanium', 11679536)])
+
+        comps = [(item.name, n) for item, n in self.comps_50_10]
+        comps.sort(key=lambda c: c[1])
+        self.assertEqual(comps, [('Megacyte', 2279), ('Zydrine', 9886), ('Nocxium', 41500), ('Isogen', 166176), ('Mexallon', 665255),
+            ('Pyerite', 2659969), ('Tritanium', 10638579)])
+
