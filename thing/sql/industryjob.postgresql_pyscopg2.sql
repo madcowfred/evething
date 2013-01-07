6393545cd81f8eef3fe45f048ffb60feaf0f241c
Freddie <freddie@wafflemonster.org>
2013-01-07 11:55:13 +1030

    Add another Postgres SQL file, stupid Django making time zone aware datetime columns :(

---
+++ thing/sql/industryjob.postgresql_pyscopg2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE thing_industryjob ALTER COLUMN install_time TYPE timestamp without time zone;
+ALTER TABLE thing_industryjob ALTER COLUMN begin_time TYPE timestamp without time zone;
+ALTER TABLE thing_industryjob ALTER COLUMN end_time TYPE timestamp without time zone;
+ALTER TABLE thing_industryjob ALTER COLUMN pause_time TYPE timestamp without time zone;
