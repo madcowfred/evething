ec9cd981d187b834d4ec0106809d2c93ef81368c
Freddie <freddie@wafflemonster.org>
2013-01-22 10:37:48 +1030

    Make some Celery config changes

--- evething/settings.py
+++ evething/settings.py
@@ -186,7 +186,11 @@ CELERY_DISABLE_RATE_LIMITS = True
 # Set a soft task time limit of 5 minutes
 CELERYD_TASK_SOFT_TIME_LIMIT = 300
 # Set the prefetch multiplier to 1 so super slow tasks aren't breaking everything
-CELERYD_PREFETCH_MULTIPLIER = 1
+#CELERYD_PREFETCH_MULTIPLIER = 1
+# We don't care about the results of tasks
+CELERY_IGNORE_RESULT = True
+# We do care about errors though
+CELERY_STORE_ERRORS_EVEN_IF_IGNORED = True
 # Set up our queues
 CELERY_DEFAULT_QUEUE = 'et_medium'
 CELERY_QUEUES = (
