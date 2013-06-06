e4a2aabd7cca31eda64c3e294da7e89c87e6d7e7
Freddie <freddie@wafflemonster.org>
2013-06-06 22:47:34 +0930

    Hopefully fix another strange home view error

--- thing/views/home.py
+++ thing/views/home.py
@@ -138,7 +138,7 @@ def home(request):
     # Do total skill point aggregation
     total_sp = 0
     for char in characters:
-        char.z_total_sp = char.total_sp or 0
+        char.z_total_sp = getattr(char, 'total_sp', 0)
         if 'sq' in char.z_training and hasattr(char, 'z_tskill'):
             char.z_total_sp += int(char.z_training['sq'].get_completed_sp(char.z_tskill, now, char))
 
