ccf84b8d5cede60cc7654ce06665f91703b5b640
Freddie <freddie@wafflemonster.org>
2013-08-11 15:52:17 +0930

    Change mail view JS to do the 'to' disambiguation only once, on load.

--- static/js/evething/mail.js
+++ static/js/evething/mail.js
@@ -1,8 +1,10 @@
 EVEthing.mail = {
     onload: function() {
+        // Filter changes
         $('#mail-side').on('click', '.btn, input', function() {
             setTimeout(EVEthing.mail.build_table, 1);
         });
+        $('#mail-side').on('change', 'select', EVEthing.mail.build_table);
 
         // Mail link click
         $('#mail-list-table').on('click', '.mail-link', EVEthing.mail.mail_link_click);
@@ -11,7 +13,10 @@ EVEthing.mail = {
         EVEthing.mail.resize();
 
         // Build character list
-        var options = '<option value="0">-ALL-</option><option>----------</option>';
+        var options = '<option value="0">-ALL-</option><option value="-" disabled>——————————</option>';
+        $.each(EVEthing.util.sorted_keys_by_value(EVEthing.mail.characters), function(i, character_id) {
+            options += '<option value="' + character_id + '">' + EVEthing.mail.characters[character_id] + '</option>';
+        });
         $('#filter-character').html(options);
         $('#filter-character').val(0);
 
@@ -20,6 +25,33 @@ EVEthing.mail = {
             EVEthing.mail.headers_url,
             function(data) {
                 EVEthing.mail.data = data;
+
+                for (var i = 0; i < data.messages.length; i++) {
+                    message = data.messages[i];
+
+                    if (message.to_list_id > 0) {
+                        message.to_list = EVEthing.mail.data.mailing_lists[message.to_list_id] || '*UNKNOWN LIST*';
+                    }
+                    else if (message.to_corp_or_alliance_id > 0) {
+                        var corp = EVEthing.mail.data.corporations[message.to_corp_or_alliance_id];
+                        if (corp !== undefined) {
+                            message.to_corporation = '<i class="icon-group"></i> ' + corp.name;
+                        }
+                        else {
+                            var alliance = EVEthing.mail.data.alliances[message.to_corp_or_alliance_id];
+                            if (alliance !== undefined) {
+                                message.to_alliance = '<i class="icon-hospital"></i> ' + alliance.name;
+                            }
+                            else {
+                                message.to_alliance = '<i class="icon-hospital"></i> *UNKNOWN*';
+                            }
+                        }
+                    }
+                    else {
+                        message.to_character = EVEthing.mail.data.characters[message.character_id] || '*UNKNOWN*';
+                    }
+                }
+
                 EVEthing.mail.build_table();
             }
         );
@@ -34,11 +66,7 @@ EVEthing.mail = {
             }
         });
         Handlebars.registerHelper('toText', function() {
-            var html = EVEthing.mail.corp_or_alliance_id(this);
-            if (html === '') {
-                html += EVEthing.mail.data.characters[this.character_id];
-            }
-            return new Handlebars.SafeString(html);
+            return new Handlebars.SafeString(this.to_list || this.to_alliance || this.to_corporation || this.to_character);
         });
         Handlebars.registerHelper('senderText', function() {
             return EVEthing.mail.data.characters[this.sender_id] || '*UNKNOWN*';
@@ -55,54 +83,61 @@ EVEthing.mail = {
 
     // Build mail-list-table
     build_table: function() {
-        console.log('build table');
         var template = Handlebars.getTemplate('mail_list');
 
         // Collect filter settings
         var filter_unread = $('#filter-unread').hasClass('active');
         var filter_read = $('#filter-read').hasClass('active');
-        //var filter_to_character = $('#filter-to-character').val();
+        var filter_to_character = ($('#filter-to-character').is(':checked'));
+        var filter_to_corporation = ($('#filter-to-corporation').is(':checked'));
+        var filter_to_alliance = ($('#filter-to-alliance').is(':checked'));
+        var filter_to_mailing_list = ($('#filter-to-mailing-list').is(':checked'));
+
+        var character_id = parseInt($('#filter-character').val());
 
         var html = '';
         for (var i = 0; i < EVEthing.mail.data.messages.length; i++) {
             var message = EVEthing.mail.data.messages[i];
+            var keep = true;
 
-            var keep = false;
+            // Display this based on read/unread state?
+            var keep_read = false;
             if (filter_unread && !message.read) {
-                keep = true;
+                keep_read = true;
             }
             if (filter_read && message.read) {
-                keep = true;
+                keep_read = true;
+            }
+            keep = keep_read;
+
+            // Display this based on selected character?
+            if (character_id > 0 && (
+                message.character_id !== character_id &&
+                message.to_characters.indexOf(character_id) < 0
+            )) {
+                keep = false;
             }
 
+            // Display this based on to: filters?
+            keep_to = false;
+            if ((filter_to_character && message.to_character) ||
+                (filter_to_corporation && message.to_corporation) ||
+                (filter_to_alliance && message.to_alliance) ||
+                (filter_to_mailing_list && message.to_list)) {
+                keep_to = true;
+            }
+            if (keep === true) {
+                keep = keep_to;
+            }
+
+
+            // Add to page if we should display it
             if (keep === true) {
                 html += template(message);
             }
         }
 
         $('#mail-list-table tbody').html(html);
-    },
-
-    // Handle corp_or_alliance_id ugh
-    corp_or_alliance_id: function(message) {
-        if (message.to_corp_or_alliance_id > 0) {
-            var corp = EVEthing.mail.data.corporations[message.to_corp_or_alliance_id];
-            if (corp !== undefined) {
-                return '<i class="icon-group"></i> ' + corp.name;
-            }
-            else {
-                var alliance = EVEthing.mail.data.alliances[message.to_corp_or_alliance_id];
-                if (alliance !== undefined) {
-                    return '<i class="icon-hospital"></i> ' + alliance.name;
-                }
-                else {
-                    return '*UNKNOWN*';
-                }
-            }
-        }
-        else {
-            return '';
-        }
     },
 
     mail_link_click: function(e) {
@@ -112,7 +147,6 @@ EVEthing.mail = {
 
         // save the table row for later
         var $tr = $(this).closest('tr');
-        console.log($tr);
 
         var message_id = parseInt($(this).attr('href').replace('#', ''));
         for (var i = 0; i < EVEthing.mail.data.messages.length; i++) {
@@ -125,13 +159,13 @@ EVEthing.mail = {
                 $('#mail-message-date').html(message.sent_date);
 
                 // To is annoying
-                var html = EVEthing.mail.corp_or_alliance_id(message);
-                if (html === '') {
+                var html = message.to_list || message.to_alliance || message.to_corporation;
+                if (html === undefined) {
                     var chars = [];
                     for (var i = 0; i < message.to_characters.length; i++) {
                         chars.push(EVEthing.mail.data.characters[message.to_characters[i]] || '*UNKNOWN*');
                     }
-                    html += chars.join(', ');
+                    html = chars.join(', ');
                 }
                 $('#mail-message-to').html(html);
 
