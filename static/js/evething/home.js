e9284d326b6fb64900dc1a961b568879e68c3224
Nicholas Steicke <nicholas.steicke@narthollis.net>
2014-03-11 17:38:34 +1030

    Fixed typo in Total SP to read SP not ISK

--- static/js/evething/home.js
+++ static/js/evething/home.js
@@ -246,7 +246,7 @@ EVEthing.home.animate = function(lastFrame) {
                 total_sp = total_sp + dyn_data['total_sp'];
             }
         }
-        $('output[name="total_sp"]').val(Handlebars.helpers.comma(total_sp) + ' ISK');
+        $('output[name="total_sp"]').val(Handlebars.helpers.comma(total_sp) + ' SP');
 
         for (var i in EVEthing.home.EVENTS) {
             if (EVEthing.home.EVENTS.hasOwnProperty(i)) EVEthing.home.EVENTS[i].animate(now);
