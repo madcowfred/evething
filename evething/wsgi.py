6d2301dcdafd561adf2ef346dc28f4b705850711
Eric Gillingham <Gillingham@bikezen.net>
2015-05-27 17:09:09 -0700

    Remove old djcelery syntax

--- evething/wsgi.py
+++ evething/wsgi.py
@@ -26,10 +26,6 @@ else:
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evething.settings")
 
-# Celery thing
-import djcelery
-djcelery.setup_loader()
-
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
